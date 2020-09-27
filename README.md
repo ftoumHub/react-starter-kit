30ce5b8ef1bc99b141dbfd58141aff38e744d9c2
GINON Georges <georges.ginon@gmail.com>
2020-09-27 15:23:08 +0200

    Adding To-Do App Features

---
+++ README.md
@@ -0,0 +1,11 @@
+# Pro React 16
+
+## CHAPTER 1
+
+### Your First React Application
+
+> Preparing the Developpent Environment
+
+> Creating the Project
+
+> Adding the To-Do Application Features
--- src/App.js
+++ src/App.js
@@ -19,24 +19,30 @@ export default class App extends Component {
     }
 
     createNewTodo = () => {
-        if (!this.state.todoItems.find(item => item.action === this.state.newItemText)) {
+        if (!this.state.todoItems
+                .find(item => item.action === this.state.newItemText)) {
             this.setState({
                 todoItems: [...this.state.todoItems,
-                            { action: this.state.newItemText, done: false }],
+                    { action: this.state.newItemText, done: false }],
                 newItemText: ""
             });
         }
     }
 
-    render = () => <div>
-        <h4 className="bg-primary text-white text-center p-2"> {this.state.userName}'s To Do List
-            ({this.state.todoItems.filter(t => !t.done).length} items to do)
-        </h4>
-        <div className="container-fluid"> <div className="my-1">
-            <input className="form-control"
-                value={this.state.newItemText} onChange={this.updateNewTextValue} />
-            <button className="btn btn-primary mt-1" onClick={this.createNewTodo}>Add</button>
+    render = () =>
+        <div>
+            <h4 className="bg-primary text-white text-center p-2">
+                {this.state.userName}'s To Do List
+                ({this.state.todoItems.filter(t => !t.done).length} items to do)
+            </h4>
+            <div className="container-fluid">
+                <div className="my-1">
+                    <input className="form-control"
+                        value={this.state.newItemText}
+                        onChange={this.updateNewTextValue} />
+                    <button className="btn btn-primary mt-1" 
+                        onClick={this.createNewTodo}>Add</button>
+                </div>
+            </div>
         </div>
-        </div>
-    </div>
 }
