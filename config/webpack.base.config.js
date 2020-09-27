8b7ca4bb903b1f1da3a33bcae403863303946e95
GINON Georges <georges.ginon@gmail.com>
2020-09-27 15:06:11 +0200

    first commit

---
+++ config/webpack.base.config.js
@@ -0,0 +1,28 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+  ]
+}
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//import './myStyles.scss';
+
+import bsStyles from 'bootstrap/dist/css/bootstrap.min.css';
+
+import 'bootstrap';
+import App from './App';
+
+//module.hot.accept();
+
+ReactDOM.render(<App />, document.getElementById('app'));
